8cce7616833397f7f818892e26a6956621294b40
caio <caio_triana@hotmail.com>
2021-06-29 19:18:12 -0300

    update foundations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-import styles from '../styles/home.module.scss';
+import Heade from 'next/Head'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <>
+    <Heade>
+      <title>Início | ig.news</title>
+    </Heade>
+    <h1>
       Hello World
     </h1>
+    </>
   )
 }
