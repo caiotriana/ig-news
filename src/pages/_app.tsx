f2f52a8d6b04b271a8cdc28f66c4a1a97e563460
Caio Triana <caio.triana@olist.com>
2022-02-02 00:27:37 -0300

    adding github auth at project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
-import { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import { Header } from "../components/Header";
+import { SessionProvider } from "next-auth/react";
 
-import { Header } from '../components/Header';
-
-import '../styles/global.scss';
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <> 
-    <Header />
-    <Component {...pageProps} />
-  </>
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
